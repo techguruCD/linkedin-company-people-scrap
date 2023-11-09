45775f53dae61c46792225eacc0a91d0ca0b4cf5
techguruCD <colinkong0406@yahoo.com>
2023-11-09 09:05:01 -0800

    ?

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from linkedin_scraper import Person, actions
 from selenium import webdriver
+driver = webdriver.Chrome()
+
+email = "hovdikruslan@gmail.com"
+password = "HondaRoyal0401!"
+actions.login(driver, email, password)
+person = Person("https://www.linkedin.com/in/joey-sham-aa2a50122", driver=driver)
+print(person)
