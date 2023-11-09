3be51462d8e59dd1b2088ec3f315bdfdd852c0bc
techguruCD <colinkong0406@yahoo.com>
2023-11-09 10:15:27 -0800

    ?

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ driver = webdriver.Chrome()
 email = "hovdikruslan@gmail.com"
 password = "HondaRoyal0401!"
 actions.login(driver, email, password)
-person = Person(company="Boostify", driver=driver)
+# person = Person(company="Boostify", driver=driver)
+person = Person(linkedin_url=None, name=None, about=[], experiences=[], educations=[], interests=[], accomplishments=[], company="Boostify", job_title=None, driver=None, scrape=True)
 print(person)
