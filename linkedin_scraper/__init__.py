f237f6e5605d17b46d5a4070595de09424dd8ef5
techguruCD <colinkong0406@yahoo.com>
2023-11-10 13:01:59 -0800

    ?

--- linkedin_scraper/__init__.py
+++ linkedin_scraper/__init__.py
@@ -5,8 +5,6 @@ from .company import Company
 from .jobs import Job
 from .job_search import JobSearch
 
-__version__ = "2.11.2"
-
 import glob
 modules = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
